cc95338f0152e066c33afc07132c318eb29ce8dd
Roman Lysenko <skiptomy55@gmail.com>
2020-07-26 16:49:36 +0000

    add HW-3 MongoDB

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
+const PORT = process.env.PORT || 3000;
 const { contactRouter } = require("./contact/contact.router");
 
-app.use(cors());
+const createServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true,
+    });
 
-app.use(express.json());
+    console.log("Database connection successful");
 
-app.use(morgan("tiny"));
+    app.use(cors());
 
-app.use("/contacts", contactRouter);
+    app.use(express.json());
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Server listening on port: " + process.env.PORT)
-);
+    app.use(morgan("tiny"));
+
+    app.use("/contacts", contactRouter);
+
+    app.listen(process.env.PORT || 3000, () =>
+      console.log("Server listening on port: " + process.env.PORT)
+    );
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+createServer();
